38c4a2d2c02d01946a88381dccef63c980dc10c3
Evan Kuryllo <evankuryllo@gmail.com>
2015-05-06 17:58:20 -0700

    Enemy Trigger tool. still in development. gizmos draw n trigger and spawners, spawners dont have functional code. -EK

---
+++ BushidoBear/Assets/Scripts/EnemyTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyTrigger : MonoBehaviour 
+{
+	[SerializeField]
+	private List<EnemySpawner> spawners = new List<EnemySpawner>();
+
+	public GameObject Spawners { set{spawners.Add (value.GetComponent<EnemySpawner>());} }
+
+
+
+	void OnDrawGizmos ()
+	{
+		Gizmos.color = Color.blue;
+		Gizmos.DrawCube (transform.position, Vector3.one);
+
+		foreach (EnemySpawner e in spawners)
+		{
+			if (e != null)
+			{
+				Gizmos.DrawLine(transform.position, e.transform.position);
+			}
+		}
+	}
+}
