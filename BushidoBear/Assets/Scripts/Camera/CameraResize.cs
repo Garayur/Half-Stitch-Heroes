47243c55a0990cc215411f3c6fe4e873a05c7983
Evan Kuryllo <evankuryllo@gmail.com>
2016-01-28 07:08:09 -0800

    Camera Implementation complete. camera has a limit of how far it can increase in size. -EK

---
+++ BushidoBear/Assets/Scripts/Camera/CameraResize.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraResize : MonoBehaviour
+{
+    public string tagToFollow;
+
+    private const float DISTANCE_MARGIN = 20.0f;
+
+    private List<Transform> players = new List<Transform>();
+    private Vector3 middlePoint = Vector3.zero;
+    private float distanceBetweenPlayers = 0;
+    private float cameraDistance = 0;
+    private float aspectRatio;
+    private float tanFov = 0;
+
+    void Start()
+    {
+        aspectRatio = Screen.width / Screen.height;
+        tanFov = Mathf.Tan(Mathf.Deg2Rad * Camera.main.fieldOfView / 2);
+
+        foreach(GameObject o in GameObject.FindGameObjectsWithTag(tagToFollow))
+        {
+            players.Add(o.transform);
+        }
+    }
+
+    void Update()
+    {
+        // Position the camera in the center.
+        Vector3 newCameraPos = Camera.main.transform.position;
+        newCameraPos.x = middlePoint.x;
+        Camera.main.transform.position = newCameraPos;
+
+        // Find the middle point between players.
+        Vector3 vectorBetweenPlayers = Vector3.zero;
+        foreach (Transform t in players)
+        {
+            vectorBetweenPlayers += t.position;
+        }
+        vectorBetweenPlayers = vectorBetweenPlayers / players.Count;
+        middlePoint = vectorBetweenPlayers;
+
+        // Calculate the new distance.
+        distanceBetweenPlayers = vectorBetweenPlayers.magnitude;
+        cameraDistance = (distanceBetweenPlayers / (float)players.Count / aspectRatio) / tanFov;
+
+        // Set camera to new position.
+        Vector3 dir = (Camera.main.transform.position - middlePoint).normalized;
+        Camera.main.transform.position = middlePoint + dir * (cameraDistance + DISTANCE_MARGIN);
+    }
+}
